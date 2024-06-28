4af8d562a423c03248104f5fa632ab0813059291
ayush-gupta-dev <ayushguptadev1@gmail.com>
2024-06-28 18:08:00 +0530

    added header themadme

--- init.lua
+++ init.lua
@@ -9,12 +9,19 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 local opts = {}
--- vim.opt.termguicolors = true
+ vim.opt.termguicolors = true
 
 vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#00FF00", bg = "#a9a9a9" })
-
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+      require("startup").display()
+    end
+  end,
+})
 require("vim-options")
 require("relative-number")
 require("lazy").setup("plugins")
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+-- Navigation between splits using Ctrl+Space followed by arrow keys
+vim.api.nvim_set_keymap('n', '<C-Space><Left>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Space><Down>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Space><Up>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Space><Right>', '<C-w>l', { noremap = true, silent = true })
